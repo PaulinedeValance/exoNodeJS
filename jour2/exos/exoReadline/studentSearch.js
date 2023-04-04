e32155170680a3b7ec9fb6d37ae9f1ccc402d38a
PaulinedeValance <pauline.de.valance@gmail.com>
2023-04-04 14:48:48 +0200

    ajout de commentaires

--- jour2/exos/exoReadline/studentSearch.js
+++ jour2/exos/exoReadline/studentSearch.js
@@ -1,19 +1,28 @@
+// importation du module readline
 var readline = require('readline')
 
+// tableau avec des noms d'étudiants
 const students = ["Alan", "Sonia", "Sophie"];
 
+// création interface readline
 rl = readline.createInterface(process.stdin, process.stdout)
 
+// On pose une question à l'utilisateur
 rl.question("Nom de l'étudiant svp ?", function(name) {
+    // suppression espaces et tout est mis en minuscule
     const studentName = name.trim().toLowerCase()
+    // recheche de l'étudiant correspondant
     const guessStudent = students.find(student => student.toLowerCase() === studentName);
     console.log(guessStudent);
 
+    // si l'étudiant a été trouvé dans le tableau on affiche son nom avec un message
     if(guessStudent) {
-        console.log(`${guessStudent} est bien un élève`);
+        console.log(`${studentName} est bien un élève`);
+    // sinon affiche message "étudiant non trouvé"    
     } else {
         console.log("Etudiant non trouvé");
     }
 
+    // fermeture
     rl.close()
 })
