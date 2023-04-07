c992e11e12e2b2b82d6735cbd77ffa3d81017001
PaulinedeValance <pauline.de.valance@gmail.com>
2023-04-07 11:17:52 +0200

    exo 1 jour 5

---
+++ jour5/exo1/index.js
@@ -0,0 +1,17 @@
+const pug = require('pug')
+
+const menuItems = [
+    { path: '/', title: 'Home', isActive: true },
+    { path: '/about-me', title: 'About', isActive: false },
+    { path: '/references', title: 'References', isActive: false },
+    { path: '/contact-me', title: 'Contact', isActive: false },
+];
+
+try{
+    const renderTemplate = pug.compileFile('layout.pug', { pretty: true});
+    const result = renderTemplate({menuItems});
+    console.log(result);
+}catch (err) {
+    console.log('Erreur de compilation :\n');
+    console.log(err.message);
+}
