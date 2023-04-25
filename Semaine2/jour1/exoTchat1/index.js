754cfa1e9627c468c428e64c1ce8f42a0af6472d
PaulinedeValance <pauline.de.valance@gmail.com>
2023-04-25 14:15:10 +0200

    ajout d'un deuxième user

--- Semaine2/jour1/exoTchat1/index.js
+++ Semaine2/jour1/exoTchat1/index.js
@@ -1,22 +1,46 @@
-const express = require('express')
+const express = require('express');
 const http = require('http');
-const { connect } = require('http2');
-const app = express()
-const server = http.Server(app)
-const io = require('socket.io')(server)
+const app = express();
+const server = http.Server(app);
+const io = require('socket.io')(server);
 
-const port = 9000
+const port = 9000;
 
-let connectedUsers = []
+let connectedUsers = [];
 
 io.on('connection', (socket) => {
-    connectedUsers.push('JeffB')
-    io.emit('updateUserList', connectedUsers);
-})
+  console.log(`Client ${socket.id} is connected via WebSockets`);
 
-// io.on('connection', (socket) => {
-//     console.log('Client', socket.id, 'is connected via WebSockets')
-// })
+  // Ajouter un utilisateur connecté à la liste des utilisateurs connectés
+  socket.on('newUser', (username) => {
+    connectedUsers.push(username);
+    io.emit('updateUserList', connectedUsers);
+  });
+
+  // Mettre à jour le pseudo d'un utilisateur dans la liste des utilisateurs connectés
+  socket.on('updateUsername', ({ oldUsername, newUsername }) => {
+    const index = connectedUsers.indexOf(oldUsername);
+    if (index !== -1) {
+      connectedUsers.splice(index, 1, newUsername);
+      io.emit('updateUserList', connectedUsers);
+    }
+  });
+
+  // Envoyer un message à tous les utilisateurs connectés
+  socket.on('sendMessage', ({ username, message }) => {
+    io.emit('newMessage', { username, message });
+  });
+
+  // Supprimer un utilisateur déconnecté de la liste des utilisateurs connectés
+  socket.on('disconnect', () => {
+    console.log(`Client ${socket.id} is disconnected`);
+    const index = connectedUsers.indexOf(socket.username);
+    if (index !== -1) {
+      connectedUsers.splice(index, 1);
+      io.emit('updateUserList', connectedUsers);
+    }
+  });
+});
 
 // Configuration de Pug comme moteur de template par défaut
 app.set('view engine', 'pug');
@@ -26,15 +50,13 @@ app.set('views', './views');
 
 // Définir la route pour la page d'accueil
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Ma page d\'accueil', message: 'Bienvenue sur ce chat', users: connectedUsers });
+  res.render('index', {
+    title: 'Ma page d\'accueil',
+    message: 'Bienvenue sur ce chat',
+    users: connectedUsers
   });
+});
 
 app.use(express.static('public'));
 
-
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
-
-server.listen(port, () => console.log(`✓ Le serveur écoute sur le port ${port}`))
+server.listen(port, () => console.log(`✓ Le serveur écoute sur le port ${port}`));
